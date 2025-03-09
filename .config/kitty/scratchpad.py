a798cbf53256964779c5a30fa35216c18148eadf
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2025-03-10 00:12:21 +0530

    Updated kitty config

---
+++ .config/kitty/scratchpad.py
@@ -0,0 +1,22 @@
+from kitty.boss import Boss
+from kittens.tui.handler import result_handler
+
+
+def main(args: list[str]) -> str:
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(
+    args: list[str], answer: str, target_window_id: int, boss: Boss
+) -> None:
+    tab = boss.active_tab
+    if len(tab.windows) == 1:
+        tab.new_window(cwd=tab.get_cwd_of_active_window())
+    else:
+        if tab.current_layout.name == "stack":
+            tab.last_used_layout()
+            tab.previous_window()
+        else:
+            tab.first_window()
+            tab.goto_layout("stack")
